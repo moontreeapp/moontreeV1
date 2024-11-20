2156e50021c71d813db02bd8892f88366d999b3e
Jordan Miller <Jordan.kay@gmail.com>
2024-11-19 22:57:19 -0700

    co-opted the scanner button for debugging, not getting anywhere

--- magic/lib/presentation/ui/appbar/scanner.dart
+++ magic/lib/presentation/ui/appbar/scanner.dart
@@ -1,12 +1,19 @@
+import 'dart:convert';
+
+import 'package:convert/convert.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:magic/cubits/cubit.dart';
 import 'package:magic/cubits/pane/wallet/cubit.dart';
+import 'package:magic/domain/wallet/sign.dart';
 //import 'package:magic/cubits/toast/cubit.dart';
 import 'package:magic/presentation/theme/colors.dart';
 import 'package:magic/presentation/ui/welcome/pair_with_chrome.dart';
 import 'package:magic/presentation/widgets/animations/fading.dart';
 import 'package:magic/services/services.dart';
+import 'package:magic/domain/blockchain/blockchain.dart';
+import 'package:magic/utils/logger.dart';
+import 'package:wallet_utils/wallet_utils.dart';
 
 class Scanner extends StatelessWidget {
   const Scanner({super.key});
@@ -26,14 +33,29 @@ class ScannerButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => GestureDetector(
-      onTap: () =>
-          //cubits.toast.flash(
-          //        msg: const ToastMessage(
-          //      title: 'Pair with Extension',
-          //      text: '(coming soon)',
-          //    )),
-          cubits.welcome
-              .update(active: true, child: const PairWithChromePage()),
+      onTap: () {
+        final kpWallet = KPWallet.fromWIF(
+            'L1pJZ8Uw2nSNjjvRSCSAwoUfugMaknTbFAU5yRPQ1cJHvXVaM7FD',
+            Blockchain.evrmoreMain.network);
+        final sig = kpWallet.sign('1731910549.8774903');
+        logWTF(hex.encode(sig));
+        logWTF(base64Encode(sig));
+        final em = EvrmoreMessage(message: '1731910549.8774903');
+        final s = signMessage(
+            ECPair.fromWIF(
+                'L1pJZ8Uw2nSNjjvRSCSAwoUfugMaknTbFAU5yRPQ1cJHvXVaM7FD',
+                Blockchain.evrmoreMain.network),
+            em);
+        logWTF(s);
+      },
+      //onTap: () =>
+      //cubits.toast.flash(
+      //        msg: const ToastMessage(
+      //      title: 'Pair with Extension',
+      //      text: '(coming soon)',
+      //    )),
+      //cubits.welcome
+      //    .update(active: true, child: const PairWithChromePage()),
       child: Container(
         height: 16 + screen.iconMedium + 16,
         width: 24 + screen.iconMedium,
